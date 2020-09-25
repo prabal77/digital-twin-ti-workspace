404b941be001aea4629e19689a22400a99e6ad2a
Nandi <nanp@softwareag.com>
2020-09-25 15:55:46 +0530

    updated paths

--- Database/start-cassandra.bat
+++ Database/start-cassandra.bat
@@ -1,5 +1,5 @@
 @echo off
-set JAVA_HOME=C:\dev-workspace\Digital-Twin-TI-Project\Database\jdk-1.8
-set CASSANDRA_HOME=C:\dev-workspace\Digital-Twin-TI-Project\Database\apache-cassandra-3.11.8
+set JAVA_HOME=C:\dev-workspace\digital-twin-ti-workspace\Database\jdk-1.8
+set CASSANDRA_HOME=C:\dev-workspace\digital-twin-ti-workspace\Database\apache-cassandra-3.11.8
 
 %CASSANDRA_HOME%\bin\cassandra.bat
