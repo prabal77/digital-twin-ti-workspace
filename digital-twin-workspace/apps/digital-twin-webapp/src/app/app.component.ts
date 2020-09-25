661027d1168c951a8f8865bdc71334cece1c90d8
Nandi <nanp@softwareag.com>
2020-09-25 15:49:37 +0530

    initial commit

---
+++ digital-twin-workspace/apps/digital-twin-webapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'digital-twin-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'digital-twin-webapp';
+}
---
+++ digital-twin-workspace/start.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=C:\dev-workspace\Digital-Twin-TI-Project\node-v12.18.3-win-x64;%PATH%
+npm %*
