ae1d5ad2640d93aade748517a08f02dc7fe4f77b
Nandi <nanp@softwareag.com>
2020-09-28 10:14:19 +0530

    db is mongodb now

--- Database/start-cassandra.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=C:\dev-workspace\digital-twin-ti-workspace\Database\jdk-1.8
-set CASSANDRA_HOME=C:\dev-workspace\digital-twin-ti-workspace\Database\apache-cassandra-3.11.8
-
-%CASSANDRA_HOME%\bin\cassandra.bat
