9ed553504add478a0c6feaef2cb42916052b121c
Kumar <vkum@softwareag.com>
2020-10-06 15:50:40 +0530

    Committing the initial UI layout.

--- src/digital-twin-webapp/src/app/app.module.ts
+++ src/digital-twin-webapp/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialsModule } from './modules/materials/materials.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
