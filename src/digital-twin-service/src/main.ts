2baeba54669e7984050fded79e932713f72086ad
Tej Babu Balireddi <tejbabu91@gmail.com>
2020-10-08 18:17:05 +0530

    change default port

--- src/digital-twin-service/src/main.ts
+++ src/digital-twin-service/src/main.ts
@@ -26,8 +26,8 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const globalPrefix = '';
 	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
-	const appName = process.env.APPLICATION || 'twin';
+	const port = process.env.PORT || 80;
+	const appName = process.env.APPLICATION || 'twin-local';
 	await app.listen(port, () => {
 		Logger.log(`Started "${appName}" application Listening at http://localhost:` + port + '/' + globalPrefix);
 	});
