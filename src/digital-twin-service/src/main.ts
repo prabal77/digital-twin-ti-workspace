6025fd4a0190062323e431ae29f17cfa3affc661
prabal <prabal77@gmail.com>
2020-10-09 16:03:06 +0530

    incorrect port committed

--- src/digital-twin-service/src/main.ts
+++ src/digital-twin-service/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const globalPrefix = '';
 	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
+	const port = process.env.PORT || 80;
 	const appName = process.env.APPLICATION_NAME || 'twin-local';
 	await app.listen(port, () => {
 		Logger.log(`Started "${appName}" application Listening at http://localhost:` + port + '/' + globalPrefix);
